3d8581b41cf18481b1532fed6e6a647a011c0d14
johndward01 <johndward01@gmail.com>
2023-03-30 14:41:24 -0500

    Add project files.

---
+++ ConsoleApp3/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3;
+internal class Methods
+{
+    public static bool Range(int start, int end, int numberToCheck)
+    {
+        if (numberToCheck >= start && numberToCheck <= end)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    public static void MultiplicationTable(int number)
+    {
+        for (int i = number; i > 0; i--)
+        {
+            Console.WriteLine($"{number} * {i} = {number * i}");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using ConsoleApp3;
+using System.Reflection;
+
+//var isInRange = Methds.Range(0, 10, 7);
+//Console.WriteLine(isInRange);
+
+Methods.MultiplicationTable(10);
